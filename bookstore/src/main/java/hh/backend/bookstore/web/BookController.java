310b3b4c7a55740560cf3fe6faf3a8f142de08c4
teemarom <bhu277@myy.haaga-helia.fi>
2026-01-27 19:57:46 +0200

    tehtävä aloitettu

---
+++ bookstore/src/main/java/hh/backend/bookstore/web/BookController.java
@@ -0,0 +1,13 @@
+package hh.backend.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index"; // index.html
+    }
+}
