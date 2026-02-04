a324132f347f3818fb4414979ee84b4c42e8b4ba
teemarom <bhu277@myy.haaga-helia.fi>
2026-02-04 21:39:06 +0200

    C3.3 tehty

--- bookstore/src/main/java/hh/backend/bookstore/web/BookController.java
+++ bookstore/src/main/java/hh/backend/bookstore/web/BookController.java
@@ -3,8 +3,13 @@ package hh.backend.bookstore.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import hh.backend.bookstore.domain.Book;
 import hh.backend.bookstore.domain.BookRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class BookController {
@@ -17,14 +22,39 @@ public class BookController {
         this.bookRepository = repository;
     }
 
+    // index
     @GetMapping("/index")
     public String index() {
         return "index"; // index.html
     }
 
+    // booklist
     @GetMapping("/booklist")
     public String listBooks(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist"; // booklist.html
     }
+
+    // delete book
+    @GetMapping("/delete/{bookId}")
+    public String deleteBook(@PathVariable("bookId") Long bookId, Model model) {
+        bookRepository.deleteById(bookId);
+        return "redirect:../booklist"; // uudelleenohjaa booklist.html
+    }
+
+    // add new book
+    @GetMapping("/addbook")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook"; // addbook.html
+    }
+    
+    // save new book
+    @PostMapping("/save")
+    public String save(Book book) {
+        bookRepository.save(book);
+        return "redirect:booklist";
+    }
+
+
 }
